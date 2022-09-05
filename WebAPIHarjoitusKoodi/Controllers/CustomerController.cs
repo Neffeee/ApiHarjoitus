c1f75edc3228245a550f6c5d05d0a85fdd80122c
Juha Laitinen <juha.laitinen@student.careeria.fi>
2022-09-05 15:02:38 +0300

    Lisätty datan päivitys!

--- WebAPIHarjoitusKoodi/Controllers/CustomerController.cs
+++ WebAPIHarjoitusKoodi/Controllers/CustomerController.cs
@@ -55,13 +55,54 @@ namespace WebAPIHarjoitusKoodi.Controllers
             }
             catch (Exception)
             {
-                return BadRequest("Jokin meni pieleen asiakasta lisättäesttä, ota yhteyttä kuruun!");
+                return BadRequest("Jokin meni pieleen asiakasta lisättäessä, ota yhteyttä kuruun!");
             }
             finally
             {
                 context.Dispose();
             }
         }
+
+        [HttpPut]//<-- Rest-terminologian mukaan tietojen päivitys
+        [Route("{key}")] //<-- Routemääritys asiakasavaimella key=CustomerID
+
+        public ActionResult PutEdit(string key, [FromBody] Customer newData) // Key on routessa ja toinen para (dataobjekti)tulee HTTP Bodyssä
+        {
+            NorthwindOriginalContext context = new();
+
+            try
+            {
+                Customer customer = context.Customers.Find(key);
+                if (customer != null)
+                {
+                    customer.CompanyName = newData.CompanyName;
+                    customer.ContactName = newData.ContactName;
+                    customer.ContactTitle = newData.ContactTitle;
+                    customer.Country = newData.Country;
+                    customer.Address = newData.Address;
+                    customer.City = newData.City;
+                    customer.PostalCode = newData.PostalCode;
+                    customer.Phone = newData.Phone;
+                    context.SaveChanges();
+                    return Ok(customer.CustomerId);
+
+                }
+                else
+                {
+                    return NotFound("Päivitettävää asiakasta ei löytynyt!");
+                }
+
+            }
+            catch(Exception)
+            {
+                return BadRequest("Jokin meni pieleen asiakasta päivitettäessä, ota yhteyttä kuruun!");
+            }
+            finally
+            {
+                context.Dispose();
+            }
+        }
+
         
     }
 }
